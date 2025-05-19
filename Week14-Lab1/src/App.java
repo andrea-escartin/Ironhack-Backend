889bcf1875d0ee4ce46f03d5d6b560f4820010bb
Andrea Escartin <escartinandrea@gmail.com>
2025-05-19 19:26:46 +0200

    lab5

--- Week14-Lab1/src/App.java
+++ Week14-Lab1/src/App.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -6,7 +7,39 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         // exercise1(sc);
-        int [] array = generateRandomIntArray(5);
+        // exercise2();
+        String [] names = {"Andrea", "Sara", "Marta", "Carla", "Maria", "Julia"};
+        int len = names.length;
+        String ans;
+        do {
+            int winnerId = (int) (Math.random() * len);
+            String winnerName = names[winnerId];
+            System.out.printf("And the winner is..... %s\n", winnerName);
+            System.out.print("Do you want to draw another winner? ");
+            ans = sc.nextLine();
+            ans = verifyAnswer(sc, ans, "Do you want to draw another winner? ");
+            
+        } while (isYes(ans));
+        
+
+        sc.close();
+    }
+
+    private static boolean isYes(String str) {
+        return str.equalsIgnoreCase("yes") || str.equalsIgnoreCase("y");
+    }  
+
+    private static String verifyAnswer(Scanner sc, String ans, String txt) {
+        final List<String> possibleAns = Arrays.asList("yes", "y", "no", "n");
+        while (!possibleAns.contains(ans)) {
+            System.out.print("I did not understand... " + txt);
+            ans = sc.nextLine().trim().toLowerCase();
+        }
+        return ans;
+    }
+
+    private static void exercise2() {
+        int [] array = generateRandomIntArray(20);
         int evenCount = 0;
         int oddCount = 0;
         for (int num : array) {
@@ -37,7 +70,6 @@ public class App {
 
         System.out.printf("The array %s has %d even number(s) and %d odd number(s)\n", Arrays.toString(array), evenCount, oddCount);
         System.out.printf("The even array is: %s\nThe odd array is: %s", Arrays.toString(arrayEven), Arrays.toString(arrayOdd));
-        sc.close();
     }
 
     private static void exercise1(Scanner sc) {
